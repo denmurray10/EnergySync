e1a7800c4131b0b2b31ffbd9ee391409bdac28ea
Dennis murray <dennis.murray10@gmail.com>
2025-11-22 23:11:41 +0000

    still not working, have you made any changes to the file?

--- src/components/add-event-modal.tsx
+++ src/components/add-event-modal.tsx
@@ -388,7 +388,7 @@ export function AddEventModal({ open, onOpenChange, onLogEvent, isProMember, age
                             <ArrowLeft className="mr-2 h-4 w-4" /> Back
                         </Button>
                     )}
-                    {step < totalSteps - 1 ? (
+                    {step < totalSteps -1 ? (
                         <Button type="button" onClick={handleNext}>
                             Next <ArrowRight className="ml-2 h-4 w-4" />
                         </Button>
